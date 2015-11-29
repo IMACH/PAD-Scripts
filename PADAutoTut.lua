30016f9c1642a7cb0a3132169199b69a0d10c0e4
IMACH <mr.kevinmach@gmail.com>
2015-11-29 00:22:56 -0800

    Create Basic Structure for Dungeon 1

---
+++ PADAutoTut.lua
@@ -0,0 +1,42 @@
+-- Pad Auto Tutorial Script 
+-- iPad Mini 1 Resolution 
+-- Portrait Locked Orientation 
+
+--START 
+-- insert tap accept 
+-- insert yes to agree to TOS 
+--tap anywhere main screen 
+-- click no for iCloud backup 
+-- click yes to are you sure 
+-- enter name method 
+-- select starting monster 
+-- yes for starting monster 
+
+-- delay for registering screen maybe 10 seconds 
+
+--clear the first dungeon 
+--welcome 
+tap(384,588); 
+usleep(3000000) ; 
+tap(386,570); 
+usleep(1000000);
+
+--welcome to first dungeon 
+tap(385,321); 
+usleep(500000); 
+
+-- you control these orbs  
+tap(387,871); 
+usleep(500000); 
+--write script to move and drag orb left to right
+
+ 
+tap(386,305); 
+-- drag glowing orb to left script from bottom right third orb 
+-- delay 5 sec 
+tap(387,853); 
+-- delay for 3 sec 
+tap(386,549); 
+-- run auto pad bot 
+	-- three finger tap 
+	-- then tap auto 
